2e754506ca6c78a806034eccc3c97a63b6f22368
fandrieu <fandrieu@gmail.com>
2012-07-15 21:22:17 +0200

    update to linux-3.4.2 & aufs3-3.4_20120611

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ archlinux-aur
 =============
 
 misc arch packages
+
+* aufs3 & linux-aufs_friendly are from calimeroteknik
---
+++ aufs3/mkarchive.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+kernel=3.4
+gitname="aufs3-${kernel}_$(date +%Y%m%d)"
+git clone git://aufs.git.sourceforge.net/gitroot/aufs/aufs3-standalone.git "$gitname"
+cd "$gitname"
+git checkout "origin/aufs${kernel}"
+rm -rf .git
+cd ..
+lastgitname="aufs3-${kernel}_$(grep AUFS_VERSION "$gitname/include/linux/aufs_type.h" |sed 's/^.*-\([0-9]*\)"$/\1/')"
+if [ -e "$lastgitname.tar.gz" ];then
+ echo 'Up-to-date, nothing done.'
+ rm -rf "$gitname"
+ exit
+fi
+mv "$gitname" "$lastgitname"
+tar czvf "$lastgitname.tar.gz" "$lastgitname" "$0"
