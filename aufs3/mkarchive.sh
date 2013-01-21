94d6026b87ff579f201b2bdfb576d2178c1cce54
fandrieu <fandrieu@gmail.com>
2013-01-21 18:29:21 +0100

    update to linux-3.7.3-1 & aufs3-3.7_20130114-1

--- aufs3/mkarchive.sh
+++ aufs3/mkarchive.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-kernel=3.6
+kernel=3.7
 gitname="aufs3-${kernel}_$(date +%Y%m%d)"
 git clone git://aufs.git.sourceforge.net/gitroot/aufs/aufs3-standalone.git "$gitname"
 cd "$gitname"
 git checkout "origin/aufs${kernel}"
 rm -rf .git
 cd ..
-lastgitname="aufs3-${kernel}_$(grep AUFS_VERSION "$gitname/include/linux/aufs_type.h" |sed 's/^.*-\([0-9]*\)"$/\1/')"
+lastgitname="aufs3-${kernel}_$(grep AUFS_VERSION "$gitname/include/uapi/linux/aufs_type.h" |sed 's/^.*-\([0-9]*\)"$/\1/')"
 if [ -e "$lastgitname.tar.gz" ];then
  echo 'Up-to-date, nothing done.'
  rm -rf "$gitname"
